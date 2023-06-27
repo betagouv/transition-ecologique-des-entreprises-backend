469f16af75f9f72bb68e641cbdf87fbc5269c36c
Julien Paris <jparis.py@gmail.com>
2023-06-27 15:12:00 +0200

    add docs endpoints

--- tee_backend_test/web/api/brevo/views.py
+++ tee_backend_test/web/api/brevo/views.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter
+from tee_backend_test.web.api.brevo.schema import Message
 
 router = APIRouter()
 
+@router.post("/", response_model=Message)
+async def send_echo_message(
+    incoming_message: Message,
+) -> Message:
+    """
+    Sends echo back to user.
+
+    :param incoming_message: incoming message.
+    :returns: message same as the incoming.
+    """
+    return incoming_message
 
 @router.get("/add_contact")
 def add_contact() -> None:
---
+++ tee_backend_test/web/api/docs/__init__.py
@@ -0,0 +1,4 @@
+"""Routes for swagger and redoc."""
+from tee_backend_test.web.api.docs.views import router
+
+__all__ = ["router"]
