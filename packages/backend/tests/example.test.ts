eba3821bdf3b28cef543812a694520dfb86d6fa8
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-07-12 15:02:32 +0200

    🔵 rename test and remove consol.log

--- packages/backend/tests/example.test.ts
+++ packages/backend/tests/example.test.ts
@@ -2,25 +2,20 @@ import axios from 'axios'
 
 import golden_file_json from '../data/test_files/golden_file-siret-01.json'
 
-describe('Test golden files', () => {
-  test('Test golden files', async () => {
-    console.log('golden_file_json : ', golden_file_json)
-
+describe('Test that API response format is unchanged', () => {
+  test('Compare response data with golden file for siret 83014132100034', async () => {
     // const api_host = 'https://tee-backend-test.osc-fr1.scalingo.io'
     const api_host = 'http://localhost:3000' // local API Express - should be started before testing
     const api_path = '/api/insee/get_by_siret'
     const api_url = `${api_host}${api_path}`
-    console.log('api_url : ', api_url)
 
     const request_body = { siret: '83014132100034' }
     const postData = JSON.stringify(request_body)
-    console.log('postData : ', postData)
 
     const headers = {
       accept: 'application/json',
       'Content-Type': 'application/json'
     }
-    console.log('headers : ', headers)
 
     const response = await axios(api_url, {
       method: 'post',
@@ -28,7 +23,6 @@ describe('Test golden files', () => {
       data: postData
     })
     const respData = response.data
-    console.log('respData : ', respData)
 
     expect(respData).toEqual(golden_file_json)
   })
