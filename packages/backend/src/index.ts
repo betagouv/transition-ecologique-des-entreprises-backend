2964bc0431e261fc00c9b018df894638da513bc6
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-07-12 11:33:47 +0200

    WIP Express get_by_siret API endpoint

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,27 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
 import { helloWorldRoute } from './routes'
+
+const environment = process.env
 
 const app: Express = express()
 const port: number = 3000
 
+app.set('env', environment)
+
 app.get('/', helloWorldRoute)
+
+app.post('/api/insee/get_by_siret', (req: Request, res: Response): void => {
+  // fetch request siret parameter
+  const siret = req.body.siret
+  console.log(siret)
+
+  console.log(req.app.get('env'))
+
+  // get token from environment
+  // build header
+  // api sirene call
+  // send response
+})
 
 app.listen(port)
 
