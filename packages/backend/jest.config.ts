4f79cf77cd8aa57ab011df5f2f811e3a469e9fa5
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2023-07-17 12:23:59 +0200

    🔵 remove comments

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -1,21 +1,10 @@
 import type { Config } from '@jest/types'
 
-// Sync object
 const config: Config.InitialOptions = {
   verbose: true,
   testEnvironment: 'node',
-  // moduleDirectories: [
-  //   "<rootDir>/node_modules",
-  //   // "../../node_modules",
-  //   // "node_modules"
-  // ],
-  // transformIgnorePatterns: [
-  //   // "node_modules/(?!variables/.*)",
-  //   "node_modules"
-  // ],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest'
-  },
-  // preset: 'ts-jest'
+  }
 }
 export default config
